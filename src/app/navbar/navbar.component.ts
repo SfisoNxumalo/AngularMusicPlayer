1326d3a55fef85e032105194ab5e91be0ed15ad2
Sfiso Mawila Nxumalo <sfisonxumalo69@gmail.com>
2023-09-26 09:49:51 +0200

    commit

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+
+  image = "https://media.istockphoto.com/id/620737858/photo/cape-town-and-the-12-apostels-from-above.jpg?s=612x612&w=is&k=20&c=WBxI8OmAFXoGz5I5QjU0eI87I5C6K8h0Xs4JmArrEQQ=";
+
+}
